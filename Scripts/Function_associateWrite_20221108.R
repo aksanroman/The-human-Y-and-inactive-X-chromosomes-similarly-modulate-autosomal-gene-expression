d40110730bd55a25e0e2567db6f0a90c55920447
aksanroman <83032231+aksanroman@users.noreply.github.com>
2023-06-05 18:27:40 -0400

    Delete Function_associateWrite_20221108.R

--- Scripts/Function_associateWrite_20221108.R
+++
@@ -1,5 +0,0 @@
-suppressPackageStartupMessages(library(dplyr))
-associateWrite <- function(myOrderedFile){
-  myOrderedFile_df <- as.data.frame(myOrderedFile)
-  myOrderedFile_anno <- cbind(myOrderedFile_df, geneAnno[match(rownames(myOrderedFile_df), geneAnno$Gene),])
-}
